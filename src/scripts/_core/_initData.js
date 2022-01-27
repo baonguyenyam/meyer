2a30992bf19a569ce6a4e9d18a38fe3b3c3d1f63
Nguyen Pham <baonguyenyam@gmail.com>
2022-01-27 02:03:27 -0600

    loading

--- src/scripts/_core/_initData.js
+++ src/scripts/_core/_initData.js
@@ -1,4 +1,7 @@
+// FILTER DATA BY COLOR, TYPE, RATING, PRICE... 
 function ___filterData(e, MEYER_APP) {
+	// ------------------------------------------------------------
+	// Filter by type 
 	if (getParameterByName('type')) {
 		let m = getParameterByName('type').split(',');
 		let AlltmpData = [];
@@ -13,6 +16,8 @@ function ___filterData(e, MEYER_APP) {
 		}
 		e = AlltmpData;
 	}
+	// ------------------------------------------------------------
+	// Filter by color 
 	if (getParameterByName('color')) {
 		let m = getParameterByName('color').split(',');
 		let AlltmpData = [];
@@ -29,16 +34,22 @@ function ___filterData(e, MEYER_APP) {
 		}
 		e = AlltmpData;
 	}
+	// ------------------------------------------------------------
+	// Filter by price 
 	if (getParameterByName('price')) {
 		e = e.filter(function (el) {
 			return parseInt(el.price) <= parseInt(getParameterByName('price'));
 		});
 	}
+	// ------------------------------------------------------------
+	// Filter by rating 
 	if (getParameterByName('rating')) {
 		e = e.filter(function (el) {
 			return el.rating >= parseInt(getParameterByName('rating')) && el.rating < (parseInt(getParameterByName('rating')) + 1);
 		});
 	}
+	// ------------------------------------------------------------
+	// Filter by sort 
 	if (getParameterByName('sort') && getParameterByName('sort') === 'desc') {
 		e.sort(function (a, b) {
 			return parseInt(b.price) - parseInt(a.price);
@@ -48,13 +59,24 @@ function ___filterData(e, MEYER_APP) {
 			return parseInt(a.price) - parseInt(b.price);
 		});
 	}
+	// ------------------------------------------------------------
 	// NEW DATA 
+	// ------------------------------------------------------------
 	if (e.length <= 0) {
 		___buildItemsEmpty()
 	} else {
 		$('.itemsall').text(e.length);
+		console.log('Start build items');
+		console.log(e)
+		console.log('End build items');
+		// for (const key in e) {
+		// 	if (Object.hasOwnProperty.call(e, key)) {
+		// 	}
+		// }
 	}
+	// ------------------------------------------------------------
 	// PAGING
+	// ------------------------------------------------------------
 	if (parseInt(getParameterByName('page'))) {
 		$.each(e.slice(((parseInt(getParameterByName('page')) - 1) * MEYER_APP.MEYER_DEFAULT_PAGE), (((parseInt(getParameterByName('page')) - 1) * MEYER_APP.MEYER_DEFAULT_PAGE) + MEYER_APP.MEYER_DEFAULT_PAGE)), (i, item) => {
 			___buildItems(item);
@@ -65,4 +87,6 @@ function ___filterData(e, MEYER_APP) {
 		});
 	}
 	MEYER_APP.buildPaging(MEYER_APP.MEYER_DEFAULT_PAGE, e.length);
+	$('.loading').hide();
+
 }
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,14 +1,22 @@
 // APP
+// ------------------------------------------------------------
+// Copyright (c) 2022 Nguyen Pham. All rights reserved.
+// ------------------------------------------------------------
 var MEYER_APP = {
 	// REST FULL API 
-	MEYER_REST_API: './data/data.json',
-	// MEYER_REST_API: 'https://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline',
+	// ------------------------------------------------------------
+	// MEYER_REST_API: './data/data.json',
+	MEYER_REST_API: 'https://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline',
+	// ------------------------------------------------------------
+	// MOCK-UP API SETTING
 	MEYER_DEFAULT_PAGE: parseInt(getParameterByName('showItems')) ? parseInt(getParameterByName('showItems')) : 6,
 	MEYER_CURRENT_QUERY: '&color=' + getParameterByName('color') + '&type=' + getParameterByName('type') + '&rating=' + getParameterByName('rating') + '&price=' + getParameterByName('price'),
 	MEYER_APP_PAGE: [9, 12, 15],
 	MEYER_APP_PRICE_RANGE: [0,20],
 	MEYER_APP_TYPE: ['bronzer', 'blush', 'lip_liner', 'foundation', 'eyeshadow', 'eyeliner', 'nail_polish', 'lipstick', 'mascara'],
 	MEYER_APP_COLOR: [{hex_value: '#D39D7B', colour_name: 'Caramel'},{hex_value: '#F5CAB9', colour_name: 'Classic Ivory'},{hex_value: '#B37560', colour_name: 'Coconut'},{hex_value: '#F3BEAC', colour_name: 'Creamy Natural '},{hex_value: '#E4A890', colour_name: 'Honey Beige '},{hex_value: '#F6D2BC', colour_name: 'Ivory'},{hex_value: '#E2B598', colour_name: 'Natural Beige'},{hex_value: '#515552', colour_name: 'Audacious Asphalt '},{hex_value: '#D8BFAC', colour_name: 'Barely Beige '},{hex_value: '#B89A89', colour_name: 'Bronze Truffle '},{hex_value: '#706A70', colour_name: 'Charcoal Chrome '},{hex_value: '#5C3B27', colour_name: 'Creamy Chocolate '},{hex_value: '#DDBA89', colour_name: 'Gold Rush '},{hex_value: '#8E8C86', colour_name: 'Grey Crystal '},{hex_value: '#7D6F7D', colour_name: 'Lavish Lavender '},{hex_value: '#957B83', colour_name: 'Lilac Lust '},{hex_value: '#E5AEB9', colour_name: 'Pink Parfait '},{hex_value: '#F1BEAB', colour_name: 'Nude '},{hex_value: '#F8E0D4', colour_name: 'Porcelain Ivory'},{hex_value: '#EEBAA4', colour_name: 'Pure Beige'},{hex_value: '#EFC2A1', colour_name: 'Sandy Beige '}],
+	// MOCK-UP API SETTING
+	// ------------------------------------------------------------
 	buildShowItem: () => {
 		___buildShowItem(MEYER_APP);
 	},
@@ -54,7 +62,7 @@ var MEYER_APP = {
 		);
 	},
 }
-
+// on jQuery Ready 
 jQuery(() => {
 	MEYER_APP.init();
 });
